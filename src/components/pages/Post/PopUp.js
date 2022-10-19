0e05379cb3884bf79e437fc9309df4b26b1ce0d9
dave8git <dave8git@gmail.com>
2022-10-19 19:17:25 +0200

    popup showing up :)

--- src/components/pages/Post/PopUp.js
+++ src/components/pages/Post/PopUp.js
@@ -1,20 +1,12 @@
-import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 const PopUp = props => {
-  const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={props.show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
