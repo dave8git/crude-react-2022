17e67eda29151167229ac21ea5070739953b50b6
dave8git <dave8git@gmail.com>
2022-10-19 21:14:03 +0200

    fix modal works

--- src/components/pages/Post/PopUp.js
+++ src/components/pages/Post/PopUp.js
@@ -1,31 +1,31 @@
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import React, { useState } from 'react';
+// import Button from 'react-bootstrap/Button';
+// import Modal from 'react-bootstrap/Modal';
+// import React, { useState } from 'react';
 
-const PopUp = props => {
-  const [show, setShow] = useState(false);
+// const PopUp = props => {
+//   const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
+ 
 
-  return (
-    <>
-      <Modal show={props.show}  onHide={handleClose}>
-        <Modal.Header>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary"  onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary"  onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Modal show={props.show}  onHide={handleClose}>
+//         <Modal.Header>
+//           <Modal.Title>Modal heading</Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+//         <Modal.Footer>
+//           <Button variant="secondary"  onClick={handleClose}>
+//             Close
+//           </Button>
+//           <Button variant="primary"  onClick={handleClose}>
+//             Save Changes
+//           </Button>
+//         </Modal.Footer>
+//       </Modal>
+//     </>
+//   );
+// }
 
-export default PopUp;
+// export default PopUp;
 
