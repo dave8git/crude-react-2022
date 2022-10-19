bfbf0f977285dda2445b574daf111058ce9ded29
dave8git <dave8git@gmail.com>
2022-10-19 20:54:16 +0200

    fix show doesn't work

--- src/components/pages/Post/MaxiPost.js
+++ src/components/pages/Post/MaxiPost.js
@@ -1,19 +1,26 @@
 import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import styles from './MaxiPost.module.scss';
 import PopUp from './PopUp.js';
 
 
 const MaxiPost = props =>{
+    const [show, setShow] = useState(false);
+
+    
+    const handleShow = () => setShow(true);
+
     return (
       <div>
         {/* {JSON.stringify(props)} */}
         <div className={styles.buttonsWrapper}>
             <h2>{props.title}</h2>
-            <Button element={<PopUp />} variant="primary">Delete</Button>
-            {/*<Button as={NavLink} to={'/post/' + props.id} variant="primary">Read More</Button> */}
+            <Button variant="primary" onClick={handleShow}>
+                Delete<PopUp show={show}/>
+            </Button>
         </div>
-        
+       
         <div><b>Author:</b>{props.author}</div>
         <div><b>Published:</b>{props.publishedDate}</div>
         <div>{props.content}</div>
--- src/components/pages/Post/PopUp.js
+++ src/components/pages/Post/PopUp.js
@@ -1,21 +1,24 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import React, { useState } from 'react';
 
 const PopUp = props => {
+  const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
 
   return (
     <>
-      <Modal show={props.show} onHide={handleClose}>
-        <Modal.Header closeButton>
+      <Modal show={props.show}  onHide={handleClose}>
+        <Modal.Header>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary"  onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary"  onClick={handleClose}>
             Save Changes
           </Button>
         </Modal.Footer>
