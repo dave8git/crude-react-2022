8fd862da7bc3acbee4639606d33ebc6fd199183c
dave8git <dave8git@gmail.com>
2022-10-22 22:10:57 +0200

    adding post working

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,12 +1,12 @@
 const initialState = {
     posts : [ 
         {
-            id: '1',
-            title: 'Article title',
-            shortDescription: 'Short description of the article...',
-            content: 'Main content of the article',
-            publishedDate: '02-02-2022',
-            author: 'John Doe'
+        id: '1',
+        title: 'Article title',
+        shortDescription: 'Short description of the article...',
+        content: 'Main content of the article',
+        publishedDate: '02-02-2022',
+        author: 'John Doe'
       },
       {
         id: '2',
